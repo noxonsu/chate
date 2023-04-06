3700d58c7dbfd552b4fb42f28db14b608c899aa0
marsiandeployer <129854151+marsiandeployer@users.noreply.github.com>
2023-04-06 14:31:58 +0000

    onout

---
+++ onout.js
@@ -0,0 +1,172 @@
+/*
+
+pm2 start "node onout.js"
+
+ */
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { body, validationResult } = require('express-validator');
+const fs = require('fs');
+const request = require('request');
+
+const { htmlEncode } = require('js-htmlencode');
+
+const app = express();
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+app.use(express.json());
+
+function esc_attr(str) {
+  return htmlEncode(str);
+}
+app.get('/', (req, res) => {
+  res.send(`<meta name="viewport" content="width=device-width, initial-scale=1">
+    <form method="post"  action="/submit-form">
+	Main text <br>
+      <textarea style='width:500px;height:300px' required name='main_text'></textarea>
+      <br>
+
+      <input type='submit' value='deploy test' style='size:30px'>
+	  <input type='button' onclick='alert("contact us")' value='deploy to my domain' style='size:30px'>
+    </form>
+  `);
+});
+
+app.post(
+  '/submit-form',
+  [body('main_text').notEmpty().withMessage('Please specify main text')],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+
+    const main_text = esc_attr(req.body.main_text);
+
+    let api_response;
+    let reponame = 'chate';
+    //telegram api get bit name by api key
+
+    const { exec } = require('child_process');
+    const nm = `i${new Date().getMinutes()}${new Date().getDate()}${new Date().getMonth()}${new Date().getFullYear()}`;
+    console.log('nm:' + nm);
+
+    exec(
+      `rm -rf /root/chate && cd /root/ && git clone https://github.com/marsiandeployer/${reponame} && cd chate && git checkout -b ${nm}`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec clone ${error}`);
+          return res.status(400).json({
+            error: `Failed to clone. ${error}`,
+          });
+        }
+
+        const regex = new RegExp(
+          `<div className="text-center text-gray-500 dark:text-gray-400">
+<div className="mb-2">
+  (.*?)
+  (.*?)
+</div>
+<div className="mb-2">
+  (.*?)
+  (.*?)\\.
+</div>
+<div className="mb-2">
+  {t\\(
+    '(.*?)',
+  \\)}
+</div>
+<div>
+  {t\\(
+    "(.*?)",
+  \\)}
+  <a
+    href="(.*?)"
+    target="_blank"
+    rel="noreferrer"
+    className="text-blue-500 hover:underline"
+  >
+    (.*?)
+  </a>
+</div>
+</div>`,
+          '',
+        );
+
+        const str_new = `<div className="text-center text-gray-500 dark:text-gray-400">
+<div className="mb-2">
+${main_text}
+</div>
+<div className="mb-2">
+sdf
+</div>
+<div className="mb-2">
+sdf
+</div>
+<div>
+sfd
+<a
+  href=""
+  target="_blank"
+  rel="noreferrer"
+  className="text-blue-500 hover:underline"
+>
+  sdfsdfsdf
+</a>
+</div>
+</div>`;
+
+        let filenam = 'components/Chat/Chat.tsx';
+        fs.readFile(filenam, 'utf8', function (err, data) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          const updatedData = data.replace(regex, str_new); //это строка для замены прямо в файле
+
+          fs.writeFile(`${filenam}_`, updatedData, 'utf8', function (err) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log('File updated successfully');
+          });
+        });
+
+        //load ghkey from .env
+
+        const ghkey = process.env.GHKEY;
+        console.log('load GHKEY from  .env');
+        console.log(ghkey);
+
+        exec(
+          `cd /root/${reponame} && git add . && git commit -m "replace chat welcome screen" && git push https://${ghkey}@github.com/marsiandeployer/${reponame}.git`,
+          (error, stdout, stderr) => {
+            if (error) {
+              console.error(`84: ${error}`);
+              return res.status(400).json({
+                error: '156',
+              });
+            }
+            console.log(stdout);
+            res.send(
+              `Success. Your app will be availabe at https://chate-git-${nm}-marsiandeployer.vercel.app/ in few minutes. Enjoy :) Plesae note if you send form again domain will be changed`,
+            );
+          },
+        );
+      },
+    );
+  },
+);
+
+app.listen(3010, () => {
+  console.log('Server running on port 3010');
+});
