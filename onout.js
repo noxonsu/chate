1e938234d5debe9a78fdafd10def612bdea7c734
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-04-07 17:50:38 +0000

    onoutjs yo

--- onout.js
+++ onout.js
@@ -10,19 +10,9 @@ if (!process.argv[2]) {
   return console.log('GHKEY not found');
 }
 
-*/
-//load GHKEY from .env
-
-console.log(process.argv[2])
-if (!process.argv[2]) {
-  return console.log('GHKEY not found');
-}
 
 const express = require('express');
-const dotenv = require('dotenv');
 
-dotenv.config();
-//load env
 
 const { body, validationResult } = require('express-validator');
 const fs = require('fs');
@@ -41,9 +31,37 @@ app.use(express.json());
 function esc_attr(str) {
   return htmlEncode(str);
 }
+
+
+let filenam = 'components/Chat/Chat.tsx';
+//test regex is it possible to change such file? does it have something to change?
+const regex = new RegExp(`<div className="text-center text-4xl font-bold text-black dark:text-white">
+              (.*?)
+            </div>`, 'gm')
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
   console.log('get /');
   console.log(process.argv[2]);
+  fs.readFile(filenam, 'utf8', function (err, data) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    //test regex is working
+    const matches = data.match(regex);
+    console.log(matches);
+    if (!matches) {
+      return res.status(400).json({
+        error: 'Failed to find regex, please contact us (error onout.js missing regex)',
+      });
+    }
+  });
   res.send(`<meta name="viewport" content="width=device-width, initial-scale=1">
     <form method="post"  action="/submit-form">
 	Main text <br>
@@ -67,7 +85,7 @@ app.post(
       });
     }
 
-    const main_text = esc_attr(req.body.main_text);
+    let main_text = esc_attr(req.body.main_text);
 
     let api_response;
     let reponame = 'chate';
@@ -87,31 +105,17 @@ app.post(
           });
         }
 
-        
-        const regex = new RegExp(`<div className="text-center text-4xl font-bold text-black dark:text-white">
-              (.*?)
-            </div>`, 'gm')
-         
 
-        const str_new = `<div className="text-center text-4xl font-bold text-black dark:text-white">
-        ${main_text}
-      </div>`;
 
-        let filenam = 'components/Chat/Chat.tsx';
         fs.readFile(filenam, 'utf8', function (err, data) {
           if (err) {
             console.error(err);
             return;
           }
-          
-          //test regex is working
-          const matches = data.match(regex);
-          console.log(matches);
-          if (!matches) {
-            return res.status(400).json({
-              error: 'Failed to find regex, please contact us (error onout.js missing regex)',
-            });
-          }
+
+          const str_new = `<div className="text-center text-4xl font-bold text-black dark:text-white">
+        ${main_text}
+      </div>`;
 
           const updatedData = data.replace(regex, str_new); //это строка для замены прямо в файле
 
@@ -127,9 +131,9 @@ app.post(
         //load ghkey from .env
 
 
-        
+
         exec(
-          `git add . && git commit -m "replace chat welcome screen" && git push https://${process.argv[2]}@github.com/marsiandeployer/${reponame}.git && git checkout main`,
+          `git commit -a -m "replace chat welcome screen" && git push https://${process.argv[2]}@github.com/marsiandeployer/${reponame}.git && git checkout main`,
           (error, stdout, stderr) => {
             if (error) {
               console.error(`84: ${error}`);
@@ -142,7 +146,7 @@ app.post(
               `Success. Your app will be availabe at https://chate-git-${nm}-marsiandeployer.vercel.app/ in few minutes. Enjoy :) Plesae note if you send form again domain will be changed`,
             );
           },
-        ); 
+        );
       },
     );
   },
