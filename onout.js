50e1db43a0a5fc5562c00ae6946806c56649572b
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-04-10 13:12:21 +0000

    onout.js

--- onout.js
+++ onout.js
@@ -149,7 +149,39 @@ app.post(
               }
               console.log(stdout);
               res.send(
-                `Success. Your app will be availabe at https://chate-git-${nm}-marsiandeployer.vercel.app/ in few minutes. Enjoy :) Plesae note if you send form again domain will be changed`,
+                `<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                    <title>deploy</title>
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+                    <link rel="stylesheet" href="styles.css">
+                  </head>
+                  <body>
+                  
+                  
+                    <div class="container col-5">
+                Success. Your app will be availabe at https://chate-git-${nm}-marsiandeployer.vercel.app/ in few minutes. Enjoy :) Plesae note if you send form again domain will be changed . 
+                <br><Br><br>
+                Widget HTML code to embed on your site:
+                
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">
+<!-- iframe with 100% height -->
+<iframe style='border:0' src="https://chate-git-${nm}-marsiandeployer.vercel.app/" class='' id="onoutiframe" width="100%" height="100%"></iframe>
+<!-- fix 100% onoutiframe iframe height on pure js -->
+<script>
+var iframe = document.getElementById('onoutiframe');
+iframe.onload = function() {
+iframe.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+};
+</script>
+                </textarea>
+
+                </div>
+                </body>
+                </html>
+                `,
               );
             },
           );
@@ -193,7 +225,7 @@ app.get('/', (req, res) => {
   
 	
 		<div class="container col-5">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
     <form method="post"  action="/submit-form"><br><br>
     Main title:<Br>
     <input type='text' name='h1text' placeholder='Welcome!' value='Your welcome message'><Br>
