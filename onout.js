226891462e1a370b5602b8014e687883b3484d24
Noxon <2914674+noxonsu@users.noreply.github.com>
2023-04-10 12:14:58 +0000

    onout.js

--- onout.js
+++ onout.js
@@ -1,9 +1,9 @@
 /*
 
-pm2 start "node onout.js"
-
+node onout.js ghp_FsX...qn (github user key)
 */
-//load GHKEY from .env
+
+const PORT = 3020
 
 console.log(process.argv[2])
 if (!process.argv[2]) {
@@ -37,7 +37,25 @@ let filenam = 'components/Chat/Chat.tsx';
 //test regex is it possible to change such file? does it have something to change?
 const regex = new RegExp(`<div className="text-center text-4xl font-bold text-black dark:text-white">
               (.*?)
-            </div>`, 'gm')
+            </div>
+            <div className="text-center text-lg text-black dark:text-white">
+              <div className="mb-8">{\`Chatbot UI is an open source clone of OpenAI\'s ChatGPT UI.\`}</div>
+              <div className="mb-2 font-bold">
+                .*?
+              </div>
+            </div>
+            <div className="text-center text-gray-500 dark:text-gray-400">
+              .*?
+              <div className="mb-2">
+                (.*?)
+              </div>
+              <div>
+                (.*?)
+              </div>
+            </div>`, 's')
+
+
+
 
 
 
@@ -62,15 +80,48 @@ app.get('/', (req, res) => {
       });
     }
   });
-  res.send(`<meta name="viewport" content="width=device-width, initial-scale=1">
-    <form method="post"  action="/submit-form">
-	Main text <br>
-      <textarea style='width:500px;height:300px' required name='main_text'></textarea>
-      <br>
 
+  //lorem ipsum for test purposes only
+  res.send(`
+  <!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>deploy</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+  
+	
+		<div class="container col-5">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <form method="post"  action="/submit-form">
+    Main title:<Br>
+    <input type='text' name='open_ai_key' placeholder='Welcome!' value='Your welcome message'><Br>
+
+	    Main text <br>
+      <textarea style='width:500px;height:300px' required name='main_text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</textarea>
+      <br>
+      OPEN_AI_KEY (leave empty if you plan to sell it externally):<Br>
+      <input type='text' name='open_ai_key' value=''><Br>
+      <Br>Link (leave empty if you plan to sell it externally):<Br>
+      <input type='text' name='link' placeholder='https://www.buymeacoffee.com/onoutorg/e/127423' value=''><Br>
+      <Br>
+      Text for link
+      <input type='text' name='linkText' placeholder='Get API key here' value=''><Br>
+      <Br>
       <input type='submit' value='deploy test' style='size:30px'>
 	  <input type='button' onclick='alert("contact us")' value='deploy to my domain' style='size:30px'>
     </form>
+
+    </div>
+    <div class="container col-5">
+    <img src=''>
+    </div>
+  </body>
+</html>
   `);
 });
 
@@ -112,10 +163,30 @@ app.post(
             console.error(err);
             return;
           }
+          let link = '';
+
+          if (request.body.link) {
+            link = "<a href='"+esc_attr(request.body.link)+"'>"+request.body.linkText+"</a>";
+          }
 
           const str_new = `<div className="text-center text-4xl font-bold text-black dark:text-white">
-        ${main_text}
-      </div>`;
+          ${h1text}
+        </div>
+        <div className="text-center text-lg text-black dark:text-white">
+          <div className="mb-8">{\`Chatbot UI is an open source clone of OpenAI\'s ChatGPT UI.\`}</div>
+          <div className="mb-2 font-bold">
+            .*?
+          </div>
+        </div>
+        <div className="text-center text-gray-500 dark:text-gray-400">
+          .*?
+          <div className="mb-2">
+            ${description}
+          </div>
+          <div>
+            ${link}
+          </div>
+        </div>`;
 
           const updatedData = data.replace(regex, str_new); //это строка для замены прямо в файле
 
@@ -152,6 +223,6 @@ app.post(
   },
 );
 
-app.listen(3010, () => {
-  console.log('Server running on port 3010');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
