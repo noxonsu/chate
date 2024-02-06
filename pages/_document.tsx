8b46b403b0c6545b5e8d98a9921cff9ca3828711
Noxon <2914674+noxonsu@users.noreply.github.com>
2024-02-06 11:10:03 +0000

    Add development environment script to _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,9 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        {process.env.NODE_ENV === 'development' && (
+          <script dangerouslySetInnerHTML={{ __html: ' window.sensorica_client_id = "5";window.post_id = "136"; window.chatUniqId = sensorica_client_id+"_"+post_id; window.main_title = "Welcome to Sensorica AI Presale Consultant. Ready to Guide You!";window.sensorica_openaiproxy = "https://sensorica.onout.org/wp-json/sensorica/v1/openaiapi/";' }}></script>
+        )}
       </Head>
       <body>
         <Main />
